0b567644bc3a3f269d1f716f52db2935ab7e43d3
ntxy <116345274+n-txy@users.noreply.github.com>
2022-12-30 11:59:10 +0800

    Abandoning vite build completely, more trouble than it's worth for vanilla js project.

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,14 @@
 import { defineConfig } from "vite"
 import liveReload from 'vite-plugin-live-reload'
-import { copyFiles } from "./src/plugins/copy-files";
+// import { copyFiles } from "./src/plugins/copy-files";
 
 export default defineConfig({
     plugins: [
 		liveReload('./src/js/*.js'),
-        copyFiles('./src/html', './dist/html'),
-        copyFiles('./src/images', './dist/images'),
-		copyFiles('./src/js', './dist/js'),
-		copyFiles('./src/styles', './dist/styles'),
+        // copyFiles('./src/html', './dist/html'),
+        // copyFiles('./src/images', './dist/images'),
+		// copyFiles('./src/js', './dist/js'),
+		// copyFiles('./src/styles', './dist/styles'),
     ],
-	root: 'src',
-	build: {
-		outDir: '../dist',
-		emptyOutDir : true
-	},
-	publicDir: '../public'
+	root: 'src'
 });
